992f2d4ec01c43b109ceea8c9c094dea0b9c1fcb
Patrik Persson <Patrik.2.Persson@sigma.se>
2018-01-25 16:29:01 +0100

    Write more code

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# react_api_mashup
-This is a API-mashup in React
+# Movie Seach App
+This is a API-mashup in React.
+used APIs
+OmDbs Movie Database API
+YouTubes API V3
--- src/App.js
+++ src/App.js
@@ -1,68 +1,80 @@
 import React, { Component } from 'react';
-//import logo from './logo.svg';
 import './App.css';
-//import Youtube from './Youtube';
 
-const API = 'AIzaSyAOYG1Ai4mZy6L-ifZgQ8bzS87vA6v3JdA'
-//const channelID = 'UCXgGY0wkgOzynnHvSEVmE3A'
-//const searchQuery = 'Jaws: The Revenge (1987) trailer'
-const searchQuery = ''
-const result = 5;
-
-// https://www.googleapis.com/youtube/v3/search?key=AIzaSyAOYG1Ai4mZy6L-ifZgQ8bzS87vA6v3JdA&channelId=UCXgGY0wkgOzynnHvSEVmE3A&part=snippet,id&order=date&maxResults=10
-
-//https://www.googleapis.com/youtube/v3/search?key=AIzaSyAOYG1Ai4mZy6L-ifZgQ8bzS87vA6v3JdA&part=snippet&q='star wars new hope',id&order=date&maxResults=10
-
-//var finalURL = `https://www.googleapis.com/youtube/v3/search?key=${API}&channelId=${channelID}&part=snippet,id&order=date&maxResults=${result}`
-
-//var finalURL = `https://www.googleapis.com/youtube/v3/search?key=${API}&part=snippet&q=${searchQuery},id&order=relevance&maxResults=${result}`
+const ytApiKey = 'AIzaSyAOYG1Ai4mZy6L-ifZgQ8bzS87vA6v3JdA'
+const ytResult = 5;
+const omdbApiKey = '99ad3b53'
 
 class App extends Component {
-  constructor(props){
-    super(props);
-
+  
+  constructor() {
+    super();
     this.state = {
-      resultyt: []
+      movies: [],
+      resultyt: [],
+      title: 'Movie Search'
     };
-    this.clicked = this.clicked.bind(this);
+    this.searchClick = this.searchClick.bind(this);
+    this.movieClick = this.movieClick.bind(this);  
   }
-clicked(){
-  event.preventDefault();
-  let searchQuery = this.refs.movieSearch.value;
-  var finalURL = `https://www.googleapis.com/youtube/v3/search?key=${API}&part=snippet&q=${searchQuery},id&order=relevance&maxResults=${result}`;
-  fetch(finalURL)
+  
+  
+  searchClick() {
+    event.preventDefault();
+    let query = this.refs.query.value;
+    let omdbUrl =`http://www.omdbapi.com/?apikey=${omdbApiKey}&s="${query}"&type=movie`;
+    let that = this;
+    fetch(omdbUrl)
       .then((response) => response.json())
       .then((responseJson) => {
         // console.log(responseJson);
+        const resultOmdb = responseJson;
+        that.setState({
+          movies: resultOmdb.Search
+      });
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+      this.refs.query.value = '';   
+  }
+
+  movieClick(ev) {
+    var movieYear = ev.target.value; 
+    var movieTitle = ev.target.dataset.name;
+    var ytUrl = `https://www.googleapis.com/youtube/v3/search?q=${movieTitle} ${movieYear} trailer&order=relevance&part=snippet&type=video&maxResults=${ytResult}&key=${ytApiKey}`;
+    fetch(ytUrl)
+      .then((response) => response.json())
+      .then((responseJson) => {
         const resultyt = responseJson.items.map(obj => "https://www.youtube.com/embed/"+obj.id.videoId);
         this.setState({resultyt});
       })
       .catch((error) => {
         console.error(error);
-      });
-}
+      });    
+  }
 
-
-
-  render(){
-    // console.log(finalURL);
-    console.log(this.state.resultyt);
-
-    return(
+  render() {
+    let title = this.state.title;
+    var movies = this.state.movies.map(function(movie,i){
+      return <a href="#" key={i}><li   data-name={movie.Title} value={movie.Year}>{movie.Title} {movie.Year}</li></a>
+    });
+    return (
       <div>
-        <input type="text" ref="movieSearch" placeholder="Write a movie" />
-        <button onClick={this.clicked}>Get youtube videos</button>
-          {
-            this.state.resultyt.map((link, i) => {
-              console.log(link);
-              var frame = <div key={i} className="youtube"><iframe  width="560" height="315" src={link} frameBorder="0" allowFullScreen></iframe></div>
-              return frame;
-            })
-          }
-          {this.frame}
-
-
-    </div>
+        <h1>{title}</h1>
+        <input className="app_input" ref="query" type="text"/>
+        <button onClick={this.searchClick}>Search</button>
+        <ul onClick={this.movieClick}>
+          {movies}
+        </ul>
+        {
+          this.state.resultyt.map((link, i) => {
+            var frame = <div key={i} className="youtube"><iframe  width="560" height="315" src={link} frameBorder="0" allowFullScreen></iframe></div>
+            return frame;
+          })
+        }
+        {this.frame}
+      </div>
     );
   }
 }
